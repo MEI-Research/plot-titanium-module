fec2d6dcf5ec42620e434f83a56eb83331473db5
Mark Mendel <mmendel@meinergy.com>
2022-07-26 17:28:14 -0500

    Update to plotproject  3.17.0.  huawei dependencies removed. minCompileSdk 30

--- android/build.sh
+++ android/build.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+GITINFO=$( git describe --long --abbrev=6 --dirty=+ )
+( cat manifest.template; \
+  sed -e "s#\\\$GITINFO#$GITINFO#" ../manifest.shared
+) > manifest
+
 echo '[mei] Building Android Plot Projects module via Titanium SDK'
 
 
-TI_SDK_VERSION="9.3.0.GA"
-NODE_VERSION=10
+TI_SDK_VERSION="9.3.2.GA"
+NODE_VERSION=14
 
 echo '[mei] selecting node version via `n`'
 n $NODE_VERSION
 
-echo '[mei] building via Ti sdk'
+rm -rf dist build
+
+echo `date` '[mei] building via Ti sdk'
 ti build  -p android -c --build-only --sdk $TI_SDK_VERSION
 
 echo ''
-echo '[mei] build complete'
+echo `date` '[mei] build complete'
