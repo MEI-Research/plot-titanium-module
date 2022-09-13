d6afb59016a0108d5238ae861ee6662f3a8dbf72
Mark Mendel <mmendel@meinergy.com>
2022-09-13 11:59:33 -0500

    Uses newer build process & Ti SDK

--- ios/build.sh
+++ ios/build.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+GITINFO=$( git describe --long --abbrev=6 --dirty=+ )
+( cat manifest.template; \
+  sed -e "s#\\\$GITINFO#$GITINFO#" ../manifest.shared
+) > manifest
 echo '[mei] Building iOS Plot Projects module via Titanium SDK'
 
 
 TI_SDK_VERSION="9.3.2.GA"
-NODE_VERSION=10
+NODE_VERSION=14
 
 echo '[mei] selecting node version via `n`'
 n $NODE_VERSION
 
+rm dist/*
+
 echo '[mei] building via Ti sdk'
-ti build  -p ios -c --build-only --sdk $TI_SDK_VERSION
+ti build  -p ios -c --build-only --sdk $TI_SDK_VERSION $*
 
 echo ''
 echo '[mei] build complete'
